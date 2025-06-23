333c2cd050d0a7021aefbcd1e230783ee4912630
Phillip Davis <phdavis1027@gmail.com>
2025-06-23 11:57:30 -0400

    feat: add creation of Definition entries in refreshdict command

--- sabaq_server/api/management/commands/refreshdict.py
+++ sabaq_server/api/management/commands/refreshdict.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from api.lib.free_dictionary_client import FreeDictionaryClient
-from api.models import Language
+from api.models import Language, DictionaryEntry, Definition, FrenchDefinitionSource
 
 from frenetic import FreNetic
 
@@ -23,3 +23,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         fwn = FreNetic(os.path.join(settings.BASE_DIR,
                        'api/static/wolf-1.0b4.xml'))
+        # Create Definitions for each DictionaryEntry
+        entries = DictionaryEntry.objects.all()
+        for entry in entries:
+            Definition.objects.create(
+                dictionary_entry=entry,
+                source=FrenchDefinitionSource.WOLF_WORDNET,
+                confidence=1.0,
+                usage_count=0
+            )
